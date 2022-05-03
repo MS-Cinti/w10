bf85e1293221d4ee33db83cdbe85025dc8c7592f
MS-Cinti <szabadicintia@gmail.com>
2022-05-03 10:13:37 +0200

    added new example for practice

--- copy.js
+++ copy.js
@@ -3,11 +3,11 @@ const arr2 = [3, 22, 70];
 
 const arr3 = [...arr1];  //mindig olyan jelek közé, amilyet éppen másolni szeretnénk (tömb, obj stb.)
 arr3.push(6);
-console.log("arr3: ", arr3);
-console.log("arr1: ", arr1);
+//console.log("arr3: ", arr3);
+//console.log("arr1: ", arr1);
 
 const arr4 = [...arr1, ...arr2]; //tömbök mergelése
-console.log("arr4: ", arr4);
+//console.log("arr4: ", arr4);
 
 const userProfile = {
     name: "Cintia",
@@ -17,6 +17,43 @@ const userProfile = {
 
 //hozzáad egy új párt valamint átírja a nevet
 const userProfile2 = {...userProfile, gender: "female", name: "Bianka"}; 
-console.log("userProfile: ", userProfile)
-console.log("userProfile2: ", userProfile2);
+//console.log("userProfile: ", userProfile)
+//console.log("userProfile2: ", userProfile2);
 
+
+
+
+// saját gyakorlás:
+
+const fruits = ["alma", "körte", "citrom", "narancs"]
+const vegetables = ["paprika", "paradicsom", "hagyma", "retek"]
+
+//lemásoljuk csak az egyik tömböt: 
+const copyArray = [...vegetables]
+//console.log(copyArray)
+
+//lemásoljuk mindegyiket: 
+const mergedArrays = [...vegetables, ...fruits]
+//console.log(mergedArrays)
+
+const myHusband = {
+    name: "Gábor",
+    age: 29, 
+    profession: "QA"
+}
+
+//lemásoljuk az objektumot
+const myHusbandCopy = {...myHusband}
+//console.log(myHusbandCopy)
+
+//lemásoljuk és adunk hozzá új kulcsot:
+const myHusbandCopyAndAddNewKey = {...myHusband, gender: "male"}
+//console.log(myHusbandCopyAndAddNewKey)
+
+//lemásoljuk és átírunk egy értéket:
+const myHusbandCopyAndChangeValue = {...myHusband, age: 30}
+//console.log(myHusbandCopyAndChangeValue)
+
+//lemásoljuk, átírunk és hozzáadunk új kulcsot/értéket:
+const myHusbandCopyAndAddNewKeyAndChangeValue = {...myHusband, age: 31, hobby: "reading"}
+console.log(myHusbandCopyAndAddNewKeyAndChangeValue)
